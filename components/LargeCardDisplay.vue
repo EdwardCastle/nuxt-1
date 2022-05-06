3b1c928e22cedf0a999500482f2a4b5a59577081
edwardcastle <sir.edwardcastle@gmail.com>
2022-05-05 23:56:12 -0400

    feat: Created card components

---
+++ components/LargeCardDisplay.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+      <b-container>
+        <h4 class="header">
+
+        </h4>
+      </b-container>
+    </div>
+</template>
+
+<script>
+    export default {
+    }
+</script>
+
+<style scoped>
+
+</style>
