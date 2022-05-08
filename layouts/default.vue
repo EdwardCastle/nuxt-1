ddf914adfc72456fa844df79398c95c4abb133d5
edwardcastle <sir.edwardcastle@gmail.com>
2022-05-07 20:10:36 -0400

    feat: added some invoices index page

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,39 @@
 <template>
   <div>
-    <div class="app flex flex-column">
+    <div v-if="!mobile" class="app flex flex-column">
       <Navigation/>
+      <Nuxt/>
     </div>
-    <Nuxt/>
+    <div v-else class="mobile-message flex flex-column">
+      <h2>Sorry this app is not supported on Mobile devices</h2>
+      <p>To use this app please use a computer or a Table</p>
+    </div>
   </div>
 </template>
 
 <script>
   export default {
-    components: {}
+    data() {
+      return {
+        mobile: false,
+        tablet: false,
+        desktop: false,
+      }
+    },
+    created() {
+      if (process.client) {
+        this.checkScreen()
+        window.addEventListener("resize", this.checkScreen)
+      }
+    },
+    methods: {
+      checkScreen() {
+        const windowWidth = window.innerWidth
+        this.mobile = windowWidth < 768;
+        this.tablet = windowWidth >= 768 && windowWidth < 1024
+        this.desktop = windowWidth > 1024
+      }
+    }
   }
 </script>
 <style lang="scss" scoped>
