6ffdb4d31930214cda4d5dd269dd705800d35c50
edwardcastle <sir.edwardcastle@gmail.com>
2022-05-13 22:40:51 -0400

    feat: added images in case no invoices

--- pages/index.vue
+++ pages/index.vue
@@ -24,8 +24,13 @@
         </div>
       </div>
     </div>
-    <div>
+    <div v-if="invoiceData.length > 0">
       <Invoice v-for="(invoice, index) in invoiceData" :invoice="invoice" :key="index"/>
+    </div>
+    <div v-else class="empty flex flex-column">
+      <img src="@/assets/images/illustration-empty.svg" alt="">
+      <h3>There is nothing here</h3>
+      <p>Create a new invoice by clicking the New Invoice button and get started</p>
     </div>
   </div>
 </template>
@@ -130,6 +135,29 @@
           }
         }
       }
+    }
+  }
+
+  .empty {
+    margin-top: 160px;
+    align-items: center;
+
+    img {
+      width: 214px;
+      height: 200px;
+    }
+
+    h3 {
+      font-size: 20px;
+      margin-top: 40px;
+    }
+
+    p {
+      text-align: center;
+      max-width: 224px;
+      font-size: 12px;
+      font-weight: 300;
+      margin-top: 16px;
     }
   }
 </style>
